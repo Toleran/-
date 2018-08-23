642af433a143fccd5dffa58486739c78a891ffc2
Toleran <872879115@qq.com>
2018-08-23 10:23:38 +0800

    js的初次体验

--- JS-study-sixth/js_16_message.js
+++ JS-study-sixth/js_16_message.js
@@ -1,17 +1,4 @@
 
-function insertAfter(newElement,targetElement) {
-	var parent=targetElement.parentNode;
-	if (parent.lastChild == targetElement) {
-		parent.appendChild(newElement);
-	}else {
-		Parent.insertBefore(newElement,targetElement.nextSibling)
-	}
-}
-
 document.getElementById("hello-btn").onclick=function (argument) {
-	var divBox=document.createElement('div');
-
-	var lastBox=document.getElementById("father").lastChild;
-	document.getElementById("father").insertAfter(divBox, lastBox);
-	divBox.className="mes-five clearfix";
-};
+	console.log("这是我的个人爱好");
+}
